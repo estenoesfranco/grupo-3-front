c5fd0d5fab54e06ddd2dbc06c32adcb9a1b5ed16
chtelo <chtello@escuelasproa.edu.ar>
2024-05-20 01:41:59 -0300

    casi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const path = require("path");
-const ejs = require("ejs");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
 // Defino el motor de plantillas a utilizar
@@ -8,6 +8,12 @@ app.set('view engine', 'ejs');
 // Defino la localización de mis vistas
 app.set('views', path.join(__dirname, 'views'));
 
+// Middlewares
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/Games-Scripts')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express
+const app = require('./app')
+const port = 3000;
 
 app.set('view engine', 'ejs');
 
 
-const port = 3000;
+app.get("/", (req, res) => {
+    res.render("index")
+});
+app.get('/Nivel1', (req, res) => {
+    res.render('Nivel1');
+});
+app.get("/Nivel2", (req, res) => {
+    res.render("Nivel2")
+});
+app.get("/Nivel3", (req, res) => {
+    res.render("Nivel3")
+});
+
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port} correctamente`);
