b2047ce6fdc7ac294c6830ef3b390c18654356c2
wwwbian <bdiaz@escuelasproa.edu.ar>
2024-05-28 10:47:30 -0300

    casi

--- public/server.js
+++ public/server.js
@@ -3,18 +3,23 @@ const port = 4000;
 
 app.set('view engine', 'ejs');
 
+
 app.get("/", (req, res) => {
     res.render("index")
 });
+
 app.get('/Nivel1', (req, res) => {
     res.render('Nivel1');
 });
+
 app.get("/Nivel2", (req, res) => {
     res.render("Nivel2")
 });
+
 app.get("/Nivel3", (req, res) => {
     res.render("Nivel3")
 });
+
 app.get("/login", (req, res) => {
     res.render("log-in")
 });
@@ -26,9 +31,11 @@ app.get("/registrarse", (req, res) => {
 app.get("/logged-user", (req, res) => {
     res.render("Logged-user")
 });
+
 app.use((req, res, next) => {
     res.status("404").render("404")
 });
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port} correctamente`);
 });
